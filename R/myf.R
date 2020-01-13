e1fe97a8d177a40af4a874b8ebc953c49f02dd3b
Paul Gilbert <paul.gilbert-1@ou.edu>
2020-01-13 16:05:52 -0600

    Initial file list

---
+++ R/myf.R
@@ -0,0 +1,14 @@
+#' Square Function
+#'
+#' Takes a vector and reurns a vector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+#' x=1:30; myf(x)
+myf=function(x){
+  x^2
+}
